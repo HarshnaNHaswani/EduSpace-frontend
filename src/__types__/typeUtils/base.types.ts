3f9c1e89f6ab5b2cf3a0f7c9a84d1e4f4c44c061
Harshna N Haswani <hharshna.n@gmail.com>
2022-08-12 17:40:47 +0530

    assets

---
+++ src/__types__/typeUtils/base.types.ts
@@ -0,0 +1,8 @@
+export type IdType = string;
+export type TokenType = string;
+export type SrcType = string;
+export type EmailType = string;
+export type TextType = string;
+export type ProviderParamType = { children: React.ReactNode };
+export type ToggleButtonDispatchType = () => void;
+
